4e129bf3f1ee4cbd3fd869463f888793a52b72e3
ATRS <82661604+ATRS7391@users.noreply.github.com>
2022-01-11 01:19:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-function processQuery(query) {
-	urlQuery = encodeURIComponent(query.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, ''));
-	return String(urlQuery);
-}
-
-function Calculatewithloader(name1, name2) {
-	document.getElementById('container').innerHTML = `
+function calculateWithLoader(name1, name2) {
+    document.getElementById('container').innerHTML = `
 		<div class="loadersss" id="loadersss">
 		<div class="loader"></div>
 		</div>
 		`;
-	var url = 'https://atrs-webapis.herokuapp.com/API/calculate_love/' + processQuery(name1) + '/' + processQuery(name2)
-	var per;
-	// I'm using my API because I am really bad at JavaScript, so I am taking the help of Backend (Python).
-	fetch(url).then(function(response) {
-		if (response.status != 200) {
-			showerror();
-		} else {
-			response.text().then(data => per = data).then(() => show(per));
-		}
-	}).catch(() => {
-		showerror();
-	});
+    let url = "https://atrs7391.herokuapp.com/api/v2/love_calculator?api_key=love_calculator&name1=" + encodeURIComponent(name1) + "&name2=" + encodeURIComponent(name2)
+    // I'm using my API because I am really bad at JavaScript, so I am taking the help of Backend (Python).
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if (data.success === true) {
+                show(data.results.love_score);
+                console.log(data)
+            }
+            else {
+                showError();
+            }
+        })
+        .catch(() => {
+            showError();
+        });
 
 }
 
 function restore() {
-	document.getElementById("container").innerHTML = `
+    document.getElementById("container").innerHTML = `
 		<div class="name1">
 		<input type="text" placeholder="Enter your Name " autocomplete="off" spellcheck="false" id="name1"/>
 		</div>
@@ -34,28 +35,28 @@ function restore() {
 		</div>
 		<div class="wrong" id="wrong"></div>
 		<div class="love-check-button">
-		<button onclick="getq()">Calculate</button>
+		<button onclick="getQ()">Calculate</button>
 		</div>
 		`;
 }
 
 
-function getq() {
-	var name1 = document.getElementById("name1").value;
-	var name2 = document.getElementById("name2").value;
-	var check1 = String(name1).replace(/\s/g, '');
-	var check2 = String(name2).replace(/\s/g, '');
+function getQ() {
+    let name1 = document.getElementById("name1").value;
+    let name2 = document.getElementById("name2").value;
+    let check1 = String(name1).replace(/\s/g, '');
+    let check2 = String(name2).replace(/\s/g, '');
 
-	if (check1.length == 0 || check2.length == 0) {
-		document.getElementById("wrong").innerHTML = "One or more field is empty or invalid values";
-	} else {
-		Calculatewithloader(name1, name2)
-	}
+    if (check1.length === 0 || check2.length === 0) {
+        document.getElementById("wrong").innerHTML = "One or more field is empty or invalid values";
+    } else {
+        calculateWithLoader(name1, name2)
+    }
 }
 
 
 function show(per) {
-	document.getElementById("loadersss").innerHTML = `
+    document.getElementById("loadersss").innerHTML = `
 		<div class="loaders" style="color: white; font-size: 3em;">Your love score is ` + per + `</div>
 		<div class="love-check-button" style='margin-top: 5vh;'>
 		<button onclick="restore()">Test again</button>
@@ -63,8 +64,8 @@ function show(per) {
 		`;
 }
 
-function showerror() {
-	document.getElementById("loadersss").innerHTML = `
+function showError() {
+    document.getElementById("loadersss").innerHTML = `
 		<div class="loaders" style="color: white; font-size: 3em;">Something went wrong, try later. </div>
 		<div class="love-check-button" style='margin-top: 5vh;'>
 		<button onclick="restore()">Try Later</button>
